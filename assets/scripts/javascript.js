1083028a7cfb81dac4c716d2b34085eca3b8e308
cirruselli <cirruselli@gmail.com>
2020-07-24 20:06:25 +0000

    added more gamelogic

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -4,6 +4,7 @@ const turnSFX = document.getElementById("turnSFX");
 //console.log(turnSFX);
 let hasTurnedCard = false;
 let firstCard, secondCard;
+let lockCards = false;
 
 function turnSound() {
     //play that funky music white boy...
@@ -12,6 +13,11 @@ function turnSound() {
 }
 
 function turnCard() {
+    if (lockCards) {return;
+    }
+    if (this === firstCard) {
+        return;
+    }
     //console.log("This card has turned: ",this);
     this.classList.toggle('turned');
     //Play soundeffect when turned. :)
@@ -25,11 +31,34 @@ function turnCard() {
         secondCard = this;
 
     if (firstCard.dataset.image === secondCard.dataset.image) {
+        //match!
         //Play a victory song
         firstCard.removeEventListener('click', turnCard);
         secondCard.removeEventListener('click', turnCard);
+    } else {
+        //nomatch, try again loser. - on score
+        //play sad tone... :)
+        lockCards = true;
+        setTimeout(() => {
+            firstCard.classList.remove('turned');
+            secondCard.classList.remove('turned');
+        
+        lockCards = false;
+        }, 2000);
     }
+
+function resetGameTurn() {
+    [hasTurnedCard, lockCards] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}    
     }
 }
+
+(function mix() {
+    cards.forEach(card => {
+        let randomOrder = Math.floor(Math.random() * 20);
+        card.style.order = randomOrder;
+    });
+})();
 //add a listener to all cards so I can CSS transform the cards later with sound effects.
 cards.forEach(card => card.addEventListener('click', turnCard));
