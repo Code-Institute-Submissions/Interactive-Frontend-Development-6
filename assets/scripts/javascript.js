357117f3bf2913d5f7f30a821fdcfa47530f9f14
cirruselli <cirruselli@gmail.com>
2020-07-27 13:57:09 +0000

    added applause sound when two cards matches in javascript.js

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -2,6 +2,9 @@ const cards = document.querySelectorAll(".card");
 //load Sound SFX for cardturning.
 const turnSFX = document.getElementById("turnSFX");
 //console.log(turnSFX);
+const winningSound = document.getElementById("winningSound");
+//play sound when the game is finished
+const applause = document.getElementById("applause");
 
 let hasTurnedCard = false;
 let firstCard, secondCard;
@@ -9,7 +12,7 @@ let lockCards = false;
 
 
 function newGame() {
-    //new game button
+    //new game button that reset the gameboard
     location.reload();
 }
 
@@ -17,6 +20,16 @@ function turnSound() {
     //play that funky music white boy...
     turnSFX.play();
     //console.log("Turn sound played.");
+}
+
+function playapplause () {
+    //play applause when 2 cards matches
+    applause.play();
+}
+
+function playWinningSound () {
+    //winning sound when game is finished
+    winningSound.play();
 }
 
 function turnCard() {
@@ -37,10 +50,11 @@ function turnCard() {
     } else {
         hasTurnedCard = false;
         secondCard = this;
-
+    
     if (firstCard.dataset.image === secondCard.dataset.image) {
         //match!
-        //Play a victory song
+        //Play an applause song
+        playapplause();
         firstCard.removeEventListener('click', turnCard);
         secondCard.removeEventListener('click', turnCard);
     } else {
@@ -62,11 +76,24 @@ function resetGameTurn() {
     }
 }
 
+/*function endOfGame () { 
+    //play music when game is finished
+    var i;
+    for (i = 0; i < cards.length; i++) {
+    cards[i];
+    }
+    if(cards == 19) {
+    playWinningSound ();
+    }
+};*/
+
 (function mix() {
+    //randomize the cards on the game-board
     cards.forEach(card => {
         let randomOrder = Math.floor(Math.random() * 20);
         card.style.order = randomOrder;
     });
 })();
+
 //add a listener to all cards so I can CSS transform the cards later with sound effects.
 cards.forEach(card => card.addEventListener('click', turnCard));
