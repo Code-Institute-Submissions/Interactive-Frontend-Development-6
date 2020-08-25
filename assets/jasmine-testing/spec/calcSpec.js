54656b216be46e43f61d544bfab52c3c85a6b45b
cirruselli <cirruselli@gmail.com>
2020-08-25 16:53:07 +0000

    added testing for score counter

--- assets/jasmine-testing/spec/calcSpec.js
+++ assets/jasmine-testing/spec/calcSpec.js
@@ -1,12 +1,20 @@
 describe("turnCard", function () {
 
-    beforeEach(function () {
+    /*beforeEach(function () {
         turnCard = new TurnCard();
     });
     beforeEach(function () {
         gameScore = new GameScore();
     });
 
+    beforeEach(function () {
+        addScore = new AddScore();
+    });
+
+    beforeEach(function () {
+        minusScore = new MinusScore();
+    });
+    
     describe("Checks TurnCard", function () {
         it("should exist", function () {
             expect(TurnCard).toBeDefined();
@@ -18,4 +26,61 @@ describe("turnCard", function () {
             expect(GameScore).toBeDefined();
         });
     });
+*/
+    describe("addScore", function() {
+        it("should return 50 and 100", function() {
+            var gameScore = 0;
+            addScore();
+            expect(gameScore).toBe(100);
+            expect(gameScore).toBe(50);
+            /*expect(AddScore).toBeDefined();*/
+        });
+    });
+
+
+    describe("minusScore", function() {
+        it("should return -10 and -30", function() {
+            var gameScore = 0;
+            minusScore();
+            expect(gameScore).toBe(-30);
+            expect(gameScore).toBe(-10);
+            /*expect(MinusScore).toBeDefined();*/
+        });
+    });
 });
+
+
+/*
+describe("FizzBuzz", function () {
+    beforeEach(function () {
+        FizzBuzz = new fizzBuzz();
+    });
+    describe("Checks Modulus", function () {
+        it("should exist", function () {
+            expect(fizzBuzz).toBeDefined();
+        });
+        it("should return FizzBuzz if divisible by 3 and by 5", function () {
+            var result = fizzBuzz(15);
+            expect(result).toBe("FizzBuzz");
+        });
+        it("should return Fizz if divisible by 3", function () {
+            var result = fizzBuzz(9);
+            expect(result).toBe("Fizz");
+        });
+        it("should return Buzz if divisible by 5", function () {
+            var result = fizzBuzz(25);
+            expect(result).toBe("Buzz");
+        });
+        it("should return number if not divisible by 3 or by 5", function () {
+            var result = fizzBuzz(2);
+            expect(result).toBe(2);
+        });
+        it("should return an error if we don't supply a number", function() {
+            spyOn(window, "alert");
+            var result = fizzBuzz(alert("Error!"));
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});
+
+*/
